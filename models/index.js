e10d1be75a1f262319ea38376917f3309fd471f2
mengxue xu <mxu4321@gmail.com>
2023-02-24 12:53:32 -0800

    updated seed index

--- models/index.js
+++ models/index.js
@@ -1,41 +1,41 @@
-const user = require("./User");
-const post = require("./Post");
-const comment = require("./Comment");
+const User = require("./User");
+const Post = require("./Post");
+const Comment = require("./Comment");
 
 // ------------user related----------------
 // user has many posts
-user.hasMany(post, {
+User.hasMany(Post, {
     foreignKey: "user_id",
 });
 
 // user has many comments
-user.hasMany(comment, {
+User.hasMany(Comment, {
     foreignKey: "user_id",
 });
 
 
 // ------------post related----------------
 // post has many comments, comments deleted when post is deleted
-post.hasMany(comment, {
+Post.hasMany(Comment, {
     foreignKey: "post_id",
     onDelete: "CASCADE",
     hooks: true,
 });
 
 // posts belong to user
-post.belongsTo(user, {
+Post.belongsTo(User, {
     foreignKey: "user_id",
 });
 
 // ------------comment related----------------
 // comments belong to user
-comment.belongsTo(user, {
+Comment.belongsTo(User, {
     foreignKey: "user_id",
 });
 
 // comments belong to post
-comment.belongsTo(post, {
+Comment.belongsTo(Post, {
     foreignKey: "post_id",
 });
 
-module.exports = { user, post, comment };
+module.exports = { User, Post, Comment };
