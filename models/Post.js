cd98b277474f4885ba4e2dfd5e5fb063aa50feb6
mengxue xu <mxu4321@gmail.com>
2023-02-24 12:30:56 -0800

    updated post model

---
+++ models/Post.js
@@ -0,0 +1,41 @@
+//  post title, contents, post creator’s username
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+// const User = require("./User");
+
+class Post extends Model {}
+
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        contents: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },  
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: "user",
+                key: "id",
+            },
+        },
+    },
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "post",
+    }
+);
+
+module.exports = Post;
--- utils/helpers.js
+++ utils/helpers.js
@@ -4,7 +4,7 @@ module.exports = {
     return date.toLocaleTimeString();
   },
   format_date: (date) => {
-    // getMonth() returns a number between 0 and 11, so need to add 1 to get the correct month
+    // getMonth() returns a number between 0 and 11, so add 1 to get the correct month
     return `${new Date(date).getMonth() + 1}/${new Date(
       date
     ).getDate()}/${new Date(date).getFullYear()}`;
