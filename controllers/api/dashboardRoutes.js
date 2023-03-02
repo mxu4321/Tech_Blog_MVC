ab7a87477dfc4e24f6b1fd28392bfb2794c9d246
mengxue xu <mxu4321@gmail.com>
2023-03-01 19:14:08 -0800

    updated dashboardRoutes

--- controllers/api/dashboardRoutes.js
+++ controllers/api/dashboardRoutes.js
@@ -1,3 +1,4 @@
+// ----> ⏰ this file may should renamed as userRoutes.js
 const router = require("express").Router();
 const { Post, User, Comment } = require("../../models");
 const withAuth = require("../../utils/auth");
@@ -20,6 +21,59 @@ router.get("/:id", withAuth, async (req, res) => {
     });
   } catch (err) {
     res.status(500).json(err);
+  }
+});
+
+// login route (localhost:3001/api/dashboard/login)
+router.post("/login", async (req, res) => {
+  try {
+    const userData = await User.findOne({ where: { email: req.body.email } });
+    if (!userData) {
+      res.status(404).json({ message: "No user found with this email address" });
+      return;
+    }
+    const validPassword = await userData.checkPassword(req.body.password);
+    if (!validPassword) {
+      res.status(404).json({message: "Password is incorrect"});
+      return;
+    }
+    // ----> ❓ correct?
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      res.json({ user: userData, message: "You are now logged in!" });
+      res.redirect("/dashboard");
+      res.status(200).json(userData);
+    });
+  } catch (err) {
+    res.status(500).json(err);
+    // console.log(err);
+  }
+});
+
+//logout route (localhost:3001/api/dashboard/logout)
+router.post("/logout", (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+// signup route (localhost:3001/api/dashboard/signup)
+router.post("/signup", async (req, res) => {
+  try {
+    const userData = await User.create(req.body);
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.status(200).json(userData);
+    });
+  } catch (err) {
+    res.status(400).json(err);
   }
 });
 
--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -2,7 +2,7 @@ const router = express.Router();
 const { Post, Comment } = require("../../models");
 const withAuth = require("../../utils/auth");
 
-// get all posts for homepage display
+// get all posts for homepage display (localhost:3001/api/post)
 router.get("/", async (req, res) => {
   try {
     const postData = await Post.findAll({
@@ -16,7 +16,7 @@ router.get("/", async (req, res) => {
   }
 });
 
-// get a single post by id
+// get a single post by id (localhost:3001/api/post/:id)
 router.get("/:id", withAuth, async (req, res) => {
   try {
     const postData = await Post.findByPk(req.params.id, {
@@ -30,7 +30,7 @@ router.get("/:id", withAuth, async (req, res) => {
   }
 });
 
-// get all posts by user id
+// get all posts by user id (localhost:3001/api/post/user/:id)
 router.get("/user/:id", withAuth, async (req, res) => {
   try {
     const postData = await Post.findAll({
@@ -47,10 +47,36 @@ router.get("/user/:id", withAuth, async (req, res) => {
   }
 });
 
-// create a new post
+// create a new post (localhost:3001/api/post/create)
 router.get("/create", withAuth, (req, res) => {
   try {
     res.render("new-post", { logged_in: req.session.logged_in });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// update a post (localhost:3001/api/post/update/:id)
+router.get("/update/:id", withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [{ model: Comment }],
+    });
+    const post = postData.get({ plain: true });
+    res.render("edit-post", { post, logged_in: req.session.logged_in });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// delete a post (localhost:3001/api/post/delete/:id)
+router.get("/delete/:id", withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [{ model: Comment }],
+    });
+    const post = postData.get({ plain: true });
+    res.render("delete-post", { post, logged_in: req.session.logged_in });
   } catch (err) {
     res.status(500).json(err);
   }
